ca57e697a864002549f8182dc9784a7220c7a723
Tangui Morlier <tangui@tangui.eu.org>
2016-05-16 20:24:01 +0200

    Gestion de l'entrée youtube mise en commentaire

--- bin/youtube_image.sh
+++ bin/youtube_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-rgrep embed_youtube _posts/* | sed 's/.*embed_youtube: //' | while read yt; do 
+rgrep embed_youtube _posts/* | grep -v '#' | sed 's/.*embed_youtube: //' | while read yt; do 
 	test -e "images/"$yt".jpg" || curl -s "http://i.ytimg.com/vi/"$yt"/maxresdefault.jpg" > "images/"$yt".jpg"
 	find images/ -size 1097c -exec rm '{}' ';'
         test -e "images/"$yt".jpg" || curl -s "http://i.ytimg.com/vi/"$yt"/0.jpg" > "images/"$yt".jpg" ; 
